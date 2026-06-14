a2686176b7cacd6807029ee3309992bfe7bfd369
Lotte Kaae <notanderx@gmail.com>
2026-06-14 16:04:27 +0200

    Copy generated catalog PDFs to out/ directory automatically

--- generate-catalog.mjs
+++ generate-catalog.mjs
@@ -651,6 +651,10 @@ ${processedProjects.map((p, idx) => {
     margin: { top: 0, right: 0, bottom: 0, left: 0 },
   });
 
+  if (fs.existsSync('out')) {
+    fs.copyFileSync(`public/${pdfFileName}`, `out/${pdfFileName}`);
+  }
+
   await page.close();
   fs.unlinkSync(tmpPath);
 
